a0d36ee313fe74dad4e872313215e316483eeb62
Juri Boiko <jboiko@williamhill.co.uk>
2017-03-29 10:37:17 +0200

    ddd

---
+++ client/data/eventsInPlay.js
@@ -0,0 +1,92 @@
+const eventsInplay = {
+    eventsInplay1:
+        {
+            name: 'Rep of Ireland vs Iceland',
+            nameTeam1: 'Rep of Ireland',
+            nameTeam2: 'Iceland',
+            id: 'OB_EV10899688',
+            markets: [
+                {
+                    name: 'Match Betting Live',
+                    id: 'OB_MA462998244',
+                    selections: [
+                        {
+                            name: 'Rep of Ireland',
+                            id: 'OB_OU1509959977',
+                            price: '6/5'
+                        }, {
+                            name: 'draw',
+                            id: 'OB_OU1508859978',
+                            price: '21/10'
+                        }, {
+                            name: 'Iceland',
+                            id: 'OB_OU1507759979',
+                            price: '13/5'
+                        }
+                    ]
+                }
+            ]
+        },
+
+
+    eventsInplay2:
+        {
+            name: 'France vs Spain',
+            nameTeam1: 'France',
+            nameTeam2: 'Spain',
+            id: 'OB_EV10833888',
+            markets: [
+                {
+                    name: 'Match Betting Live',
+                    id: 'OB_MA462318888',
+                    selections: [
+                        {
+                            name: 'France',
+                            id: 'OB_OU1588859977',
+                            price: '29/20'
+                        }, {
+                            name: 'draw',
+                            id: 'OB_OU1502959888',
+                            price: '2/1'
+                        }, {
+                            name: 'Spain',
+                            id: 'OB_OU1502888979',
+                            price: '21/10'
+                        }
+                    ]
+                }
+            ]
+        },
+
+
+    eventsInplay3:
+        {
+            name: 'Holland vs Italy',
+            nameTeam1: 'Holland',
+            nameTeam2: 'Italy',
+            id: 'OB_EV10833777',
+            markets: [
+                {
+                    name: 'Match Betting Live',
+                    id: 'OB_MA462318777',
+                    selections: [
+                        {
+                            name: 'Holland',
+                            id: 'OB_OU1502959777',
+                            price: '7/4'
+                        }, {
+                            name: 'draw',
+                            id: 'OB_OU1502959666',
+                            price: '11/5'
+                        }, {
+                            name: 'Italy',
+                            id: 'OB_OU1502959555',
+                            price: '13/8'
+                        }
+                    ]
+                }
+            ]
+        }
+};
+
+export default eventsInplay;
