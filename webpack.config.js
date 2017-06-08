d464a4fc14d5a64938284414ba7cc2dbda4aae7f
Juri Boiko <jboiko@williamhill.co.uk>
2017-06-08 13:44:33 +0200

    build

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+	'babel-polyfill',
+    './client/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+    plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+  module: {
+    loaders: [
+    // js
+    {
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'client')
+    },
+    // CSS
+    {
+      test: /\.styl$/,
+      include: path.join(__dirname, 'client'),
+      loader: 'style-loader!css-loader!stylus-loader'
+    }
+    ]
+  }
+};
